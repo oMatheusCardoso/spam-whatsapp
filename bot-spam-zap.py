44479509d7ea5b0394ecca53e41bff2ec7819f0d
omatheuscardoso <mattheuscardoso@aol.com>
2024-12-11 12:59:52 -0300

    ver1.1 - add button stop

--- bot-spam-zap.py
+++ bot-spam-zap.py
@@ -3,41 +3,67 @@ import tkinter as tk
 from tkinter import  messagebox
 import time
 
-def botao_spam():
-  global contador
+executando = False
+contador = 0
 
+def botao_spam():
+  global contador, executando
+
+  executando = True
   contador = 0
+
+  botao_iniciar.config(state=tk.DISABLED)
+  botao_parar.config(state=tk.NORMAL)
+
   msg = entrada_mensagem.get().strip()
 
   try:
     qtd = int(entrada_quantidade.get())
   except ValueError:
     messagebox.showerror('Por favor, insira um número inteiro.')
+    finalizar_envio()
     return
 
   if not msg:
     messagebox.showerror('Por favor, insira uma mensagem.')
+    finalizar_envio()
     return
 
   if qtd <= 0:
-    messagebox.showerror('Por favor, insira um número inteiro positivo.')
+    messagebox.showerror('Por favor, insira um número maior do que 0.')
+    finalizar_envio()
     return
 
   feedback_label.config(text='Em 10 segundos, o envio começará!\n Deixe o input do WhatsApp selecionado.')
   janela.update_idletasks()
-  time.sleep(10)
+  janela.after(10000, lambda: enviar_mensagens(msg, qtd))
 
+def enviar_mensagens(msg, qtd):
+  global contador, executando
+  if not executando or contador >= qtd:
+    finalizar_envio()
+    return
   
-  for i in range(qtd):
-    pygui.write(msg)
-    pygui.press('enter')
-    contador += 1
-    feedback_label.config(text=f'Enviado {contador} de {qtd} vezes.')
-    janela.update_idletasks()
-    time.sleep(0.3)
+  pygui.write(msg)
+  pygui.press('enter')
+  contador += 1
+  feedback_label.config(text=f'Enviado {contador} de {qtd} vezes.')
+  janela.update_idletasks()
+  janela.after(300, lambda: enviar_mensagens(msg, qtd))
 
-  feedback_label.config(text='Envio Concluído!')
+def finalizar_envio():
+  global executando
+  executando = False
   botao_iniciar.config(state=tk.NORMAL)
+  botao_parar.config(state=tk.DISABLED)
+  feedback_label.config(text='Envio concluído ou interrompido.')
+
+
+def parar_spam():
+  global executando
+  executando = False
+  feedback_label.config(text='Envio Interrompido!')
+  finalizar_envio()
 
 janela = tk.Tk()
 janela.title('Spam message for WhatsApp')
@@ -58,8 +84,11 @@ entrada_quantidade.grid(column=0, row=3, padx=10, pady=10)
 botao_iniciar = tk.Button(janela, text="Iniciar Spam", command=botao_spam)
 botao_iniciar.grid(column=0, row=4, padx=10, pady=10)
 
+botao_parar = tk.Button(janela, text="Parar Spam", command=parar_spam, state=tk.DISABLED)
+botao_parar.grid(column=0, row=5, padx=10, pady=10)
+
 feedback_label = tk.Label(janela, text='', fg='blue')
-feedback_label.grid(column=0, row=5, padx=10, pady=10, sticky='ew')
+feedback_label.grid(column=0, row=6, padx=10, pady=10, sticky='ew')
 
 janela.columnconfigure(0, weight=1)
 janela.mainloop()
